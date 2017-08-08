55519708eb57372251dfb86a16debae04efe1f7d
Bruce Lewis <blewis@fitbit.com>
2017-08-08 11:05:27 -0400

    tighter indent to fit on blog post

--- 2017/planets.jsx
+++ 2017/planets.jsx
@@ -6,77 +6,85 @@ let planets;
 let planetFilter = planet => true;
 
 class PlanetApp {
-    view() {
-        return (
-            <section>
-                <PlanetFilters />
-                <PlanetTable planets={planets} />
-            </section>
-        );
-    }
+  view() {
+    return (
+      <section>
+        <PlanetFilters />
+        <PlanetTable planets={planets} />
+      </section>
+    );
+  }
 }
 
 class PlanetTable {
-    view() {
-        return (
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Composition</th>
-                    <th>Moons</th>
-                </tr>
+  view() {
+    return (
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Composition</th>
+          <th>Moons</th>
+        </tr>
             {planets.filter(planetFilter).map(planet => <PlanetRow planet={planet} />)}
-            </table>
-        );
-    }
+      </table>
+    );
+  }
 }
 
 class PlanetRow {
-    view(vnode) {
-        const { composition, name, moons } = vnode.attrs.planet;
-        return (
-            <tr>
-                <td>{name}</td>
-                <td>{composition}</td>
-                <td>{moons}</td>
-            </tr>
-        );
-    }
+  view(vnode) {
+    const { composition, name, moons } = vnode.attrs.planet;
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{composition}</td>
+        <td>{moons}</td>
+      </tr>
+    );
+  }
 }
 
 class PlanetFilters {
-    view(vnode) {
-        return (
-            <p>
-                <PlanetFilter key="All" func={planet => true} />
-                <PlanetFilter key="Terrestrial" func={planet => planet.composition === 'terrestrial'} />
-                <PlanetFilter key="Gas Giant" func={planet => planet.composition === 'gas giant'} />
-                <PlanetFilter key="Ice Giant" func={planet => planet.composition === 'ice giant'} />
-            </p>
-        );
-    }
+  view(vnode) {
+    return (
+      <p>
+        <PlanetFilter
+            key="All"
+            func={planet => true} />
+        <PlanetFilter
+            key="Terrestrial"
+            func={planet => planet.composition === 'terrestrial'} />
+        <PlanetFilter
+            key="Gas Giant"
+            func={planet => planet.composition === 'gas giant'} />
+        <PlanetFilter
+            key="Ice Giant"
+            func={planet => planet.composition === 'ice giant'} />
+      </p>
+    );
+  }
+}
+
+class PlanetFilter {
+  view(vnode) {
+    const { key, func } = vnode.attrs;
+    return (
+      <label>
+        <input type="radio" name="filter" onchange={filterHandler(func)} /> {key}
+      </label>
+    );
+  }
 }
 
 function filterHandler(filterFunction) {
-    return function(event) {
-        if (event.target.checked) {
-            planetFilter = filterFunction;
-        }
-    };
-}
-    
-class PlanetFilter {
-    view(vnode) {
-        const { key, func } = vnode.attrs;
-        return (
-            <label>
-                <input type="radio" name="filter" onchange={filterHandler(func)} /> {key}
-            </label>
-        );
+  return function(event) {
+    if (event.target.checked) {
+      planetFilter = filterFunction;
     }
+  };
 }
 
 m.request({url: 'planets.json'}).then(data => {
-    planets = data;
-    m.mount(document.getElementById('app'), PlanetApp);
+  planets = data;
+  m.mount(document.getElementById('app'), PlanetApp);
 });
